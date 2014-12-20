ca83f10183848ea97165a73d2174fad876418e6e
Vadym Panchenko <panchenko.vadym@gmail.com>
2014-12-20 17:21:33 +0200

    Initial commit

---
+++ src/AppBundle/Entity/Student.php
@@ -0,0 +1,154 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Student
+ *
+ * @ORM\Table()
+ * @ORM\Entity
+ */
+class Student
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="first_name", type="string", length=255)
+     */
+    private $firstName;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="last_name", type="string", length=255)
+     */
+    private $lastName;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="github_login", type="string", length=255)
+     */
+    private $githubLogin;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="project", type="string", length=255)
+     */
+    private $project;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set firstName
+     *
+     * @param string $firstName
+     * @return Student
+     */
+    public function setFirstName($firstName)
+    {
+        $this->firstName = $firstName;
+
+        return $this;
+    }
+
+    /**
+     * Get firstName
+     *
+     * @return string 
+     */
+    public function getFirstName()
+    {
+        return $this->firstName;
+    }
+
+    /**
+     * Set lastName
+     *
+     * @param string $lastName
+     * @return Student
+     */
+    public function setLastName($lastName)
+    {
+        $this->lastName = $lastName;
+
+        return $this;
+    }
+
+    /**
+     * Get lastName
+     *
+     * @return string 
+     */
+    public function getLastName()
+    {
+        return $this->lastName;
+    }
+
+    /**
+     * Set githubLogin
+     *
+     * @param string $githubLogin
+     * @return Student
+     */
+    public function setGithubLogin($githubLogin)
+    {
+        $this->githubLogin = $githubLogin;
+
+        return $this;
+    }
+
+    /**
+     * Get githubLogin
+     *
+     * @return string 
+     */
+    public function getGithubLogin()
+    {
+        return $this->githubLogin;
+    }
+
+    /**
+     * Set project
+     *
+     * @param string $project
+     * @return Student
+     */
+    public function setProject($project)
+    {
+        $this->project = $project;
+
+        return $this;
+    }
+
+    /**
+     * Get project
+     *
+     * @return string 
+     */
+    public function getProject()
+    {
+        return $this->project;
+    }
+}
