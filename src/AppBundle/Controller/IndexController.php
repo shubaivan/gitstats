106c00e90c52dedfc638f12bd2c5965e2acf5594
Vadym Panchenko <panchenko.vadym@gmail.com>
2014-12-22 21:41:32 +0200

    Added action, Student FormType and twig template

--- src/AppBundle/Controller/IndexController.php
+++ src/AppBundle/Controller/IndexController.php
@@ -2,16 +2,38 @@
 
 namespace AppBundle\Controller;
 
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use AppBundle\Entity\Student;
+use AppBundle\Form\StudentType;
+use Github\Client;
 
 class IndexController extends Controller
 {
     /**
-     * @Route("/", name="homepage")
+     * @Template(template=":AppBundle/Index:index.html.twig")
+     * @Route("/")
+     * @Method(methods={"GET", "POST"})
+     *
+     * @param $request
+     * @return Student
      */
-    public function indexAction()
+    public function indexAction(Request $request)
     {
-        return $this->render('default/index.html.twig');
+        $student = new Student();
+        $form = $this->createForm(new StudentType(), $student);
+        $form->handleRequest($request);
+
+        if ($request->isMethod('POST')) {
+            if ($form->isValid()) {
+                $this->container->get('doctrine.orm.entity_manager')->persist($student);
+                $this->container->get('doctrine.orm.entity_manager')->flush();
+            }
+        }
+
+        return ["form" => $form->createView()];
     }
 }
---
+++ src/AppBundle/Form/StudentType.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace AppBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use AppBundle\Entity\Student;
+
+class StudentType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('firstName')
+            ->add('lastName')
+            ->add('githubLogin')
+            ->add('project')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'AppBundle\Entity\Student',
+            'csrf_protection' => true,
+            'intention'       => 'appbundle_student',
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'appbundle_student';
+    }
+}
